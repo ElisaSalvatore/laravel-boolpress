2adad215f6ea3c166b93f4baf354f3fc8e73d124
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-03-15 21:24:39 +0100

    Creates navbar component and ends vue initialize

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Category;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function create() {
+        $categories = Category::all();
+        
+        return view("posts.create", compact("categories"));
+    }
 }
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,22 +1,28 @@
 <template>
-  <div>
+    <div>
+      <TheNavbar></TheNavbar>
+
+        <div class="container py-4">
+            <h1 class="p-4">Benvenuto in Vue.js</h1>
+
+            <!-- <ul>
+                <li v-for="i in 10 " :key="i"> 
+                    {{ i }}
+                </li>
+            </ul>  -->
+
+            <!-- <ExampleComponentVue></ExampleComponentVue> -->
+        </div>
       
-      <h1>Benvenuto in Vue.js</h1>
-
-        <!-- <ul>
-            <li v-for="i in 10 " :key="i"> 
-                {{ i }}
-            </li>
-        </ul>  -->
-
-        <!-- <ExampleComponentVue></ExampleComponentVue> -->
-  </div>
+    </div>
 </template>
 
 <script>
 // import ExampleComponentVue from "../components/ExampleComponent.vue";
+import TheNavbar from "../components/TheNavbar.vue";
 export default {
     // components: { ExampleComponentVue },
+    components: { TheNavbar},
 };
 </script>
 
