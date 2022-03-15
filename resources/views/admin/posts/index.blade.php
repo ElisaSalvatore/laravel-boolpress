d15fc87fdc57e07fff751cd5f344d4b337a340f5
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-03-15 23:09:24 +0100

    Creates create.blade and fills create() into PostController

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,18 +1,23 @@
-@extends('layout.app')
+@extends('layouts.app')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Lista dei post</div>
+                <div class="card-header d-flex">
+                    Lista dei post
+
+                    {{-- Bottone per la creazione di un nuovo post --}}
+                    <a class="ml-4" href="{{ route("admin.posts.create") }}">Crea post</a>
+                </div>
 
                 <div class="card-body">
                     <ul class="list-group">
                         @foreach ( $posts as $post)
                             <li class="list-group-item">{{$post->title}}</li>
                         @endforeach
-                      </ul>
+                    </ul>
                 </div>
             </div>
         </div>
